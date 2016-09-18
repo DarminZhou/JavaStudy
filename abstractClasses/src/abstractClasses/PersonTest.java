8a7d6841e1bfb901fdff0b7cd8c61ef3775272b3
DarminZhou <790471332@qq.com>
2016-09-18 17:21:11 +0800

    1

---
+++ abstractClasses/src/abstractClasses/PersonTest.java
@@ -0,0 +1,21 @@
+package abstractClasses;
+
+public class PersonTest
+{
+	public static void main(String [] args)
+	{
+		Person[] people =new Person[2];
+		people[0] = new Employee("Carl Cracker",7500,1987,12,15);
+		people[1] = new Student("Harry Hacker","computer science");
+		
+		for(Person p:people)
+		 System.out.println(p.getName() +"," + p.getDescription());
+		
+	}
+
+}
+
+
+
+
+
